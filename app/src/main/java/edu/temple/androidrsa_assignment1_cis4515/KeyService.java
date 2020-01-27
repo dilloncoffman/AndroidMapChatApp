a096e587fab971de86cba6ad11fb7af97e191857
Dillon Coffman <tug40394@temple.edu>
2020-01-27 15:46:46 -0500

    Implemented getMyKeyPair() in KeyService and set up KeyService connection in MainActivity

--- app/src/main/java/edu/temple/androidrsa_assignment1_cis4515/KeyService.java
+++ app/src/main/java/edu/temple/androidrsa_assignment1_cis4515/KeyService.java
@@ -3,20 +3,20 @@ package edu.temple.androidrsa_assignment1_cis4515;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
-
-import java.io.File;
-import java.security.Key;
 import java.security.KeyPair;
-import java.security.PublicKey;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
 
 public class KeyService extends Service {
     private final KeyService.KeyBinder binder = new KeyService.KeyBinder();
+    private KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+    private KeyPair myKeyPair = null;
 
-    public KeyService() {
+
+    public KeyService() throws NoSuchAlgorithmException {
     }
 
     @Override
@@ -32,11 +32,17 @@ public class KeyService extends Service {
         same key pair
      */
     public KeyPair getMyKeyPair() {
-        return null;
+        if (myKeyPair == null) {
+            myKeyPair = keyPairGenerator.generateKeyPair();
+            return myKeyPair;
+        } else {
+            return myKeyPair;
+        }
     }
 
     /*
         Store a key for a provided partner name
+        TODO Should we store KeyPairs using KeyStore?
      */
     void storePublicKey (String partnerName, String publicKey) {
 
@@ -47,18 +53,29 @@ public class KeyService extends Service {
         provided partner name
      */
     public RSAPublicKey getPublicKey(String partnerName) {
+        // TODO Get public key associated with a specific partner in HashTable (HashTable<partnerName, partnerPublicKey>) OR should we represent a "partner" in a different way?
         return null;
     }
 
     /*
         Erases current KeyPair and replaces it with a new KeyPair
+        TODO Safe to assume resetting KeyPair also entails generating a new KeyPair?
      */
     public void resetMyKeyPair() {
-
+        // Erase current key pair
+        if (myKeyPair != null) {
+            myKeyPair = null;
+        } else {
+            Log.d("KeyService", "Currently no key pair set");
+            return;
+        }
+        // Assuming you would want to generate a new key pair
+        myKeyPair = keyPairGenerator.generateKeyPair();
     }
 
     /*
         Erases current public key for a specific partner
+        TODO Safe to assume resetting KeyPair also entails generating a new KeyPair?
      */
     public void resetKey(String partnerName) {
 
--- app/src/main/java/edu/temple/androidrsa_assignment1_cis4515/MainActivity.java
+++ app/src/main/java/edu/temple/androidrsa_assignment1_cis4515/MainActivity.java
@@ -2,13 +2,52 @@ package edu.temple.androidrsa_assignment1_cis4515;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean connected;
+    Intent keyIntent;
+    KeyService.KeyBinder keyBinder;
+    ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            connected = true;
+            Log.d("KeyServiceConnection: ", "Connected");
+            keyBinder = (KeyService.KeyBinder) service; // hold reference to KeyBinder that KeyService is returning that describes interactions you can perform
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d("KeyServiceConnection: ", "Disconnected: Service was killed for some reason");
+            connected = false; // no longer connected
+            keyBinder = null; // to protect against memory leak
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind to KeyService
+        keyIntent = new Intent(this, KeyService.class);
+        Log.d("KeyService", "Bound to service connection");
+        bindService(keyIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Unbind from KeyService
+        Log.d("KeyService", "Unbound from service connection");
+        unbindService(serviceConnection);
     }
 }
