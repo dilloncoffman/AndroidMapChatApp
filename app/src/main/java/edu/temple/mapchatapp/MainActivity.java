2c36e7f494874235b042b5e0afbc5d48b70d1ced
Dillon Coffman <tug40394@temple.edu>
2020-02-10 14:44:14 -0500

    Removed unnecessary code from MainActivity; Volley HTTP library was added on master branch; UserListFragment and MapFragment created along with their containers

--- app/src/main/java/edu/temple/mapchatapp/MainActivity.java
+++ app/src/main/java/edu/temple/mapchatapp/MainActivity.java
@@ -9,17 +9,10 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import java.security.KeyPair;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView publicKeyText;
-    TextView privateKeyText;
-    Button getKeyPairButton;
     KeyPair myKeyPair;
 
     boolean connected;
@@ -45,25 +38,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        publicKeyText = findViewById(R.id.publicKey);
-        privateKeyText = findViewById(R.id.privateKey);
-        getKeyPairButton = findViewById(R.id.getKeyPairButton);
-
         // Bind to KeyService
         keyIntent = new Intent(this, KeyService.class);
         Log.d("KeyService", "Bound to service connection");
         bindService(keyIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        myKeyPair = keyBinder.getMyKeyPair(); // hold ref to user's keyPair
 
-        getKeyPairButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startService(keyIntent);
-                myKeyPair = keyBinder.getMyKeyPair();
-                publicKeyText.setText(String.valueOf(myKeyPair.getPublic()));
-                privateKeyText.setText(String.valueOf(myKeyPair.getPrivate()));
-            }
-        });
     }
 
     @Override
